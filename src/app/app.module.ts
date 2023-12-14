448a1051959a7a2679f2e247b1da4caa362fdec2
riefive <muhammad.rifai@salt.co.id>
2023-12-14 11:14:36 +0700

    feat(lazy-image): add directive image lazy load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ErrorMessageComponent } from './components/commons/error-message.component';
 import { SecurityComponent } from './components/security/security.component';
+import { DemoImageComponent } from './components/demo-image/demo-image.component';
+import { LazyImageDirective } from './directives/lazy-image.directive';
+import { TextHighlightDirective } from './directives/text-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
     ErrorMessageComponent,
-    SecurityComponent
+    SecurityComponent,
+    DemoImageComponent,
+    LazyImageDirective,
+    TextHighlightDirective
   ],
   imports: [
     FormsModule,
