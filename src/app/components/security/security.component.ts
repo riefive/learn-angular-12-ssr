7b12f896d0fc1cc434cb8d04f98fb2322e60564c
riefive <muhammad.rifai@salt.co.id>
2023-12-14 13:34:28 +0700

    feat(sanitize): implement sanitize url

--- src/app/components/security/security.component.ts
+++ src/app/components/security/security.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import { of } from 'rxjs';
 
 @Component({
@@ -9,20 +9,30 @@ import { of } from 'rxjs';
 })
 export class SecurityComponent implements OnInit {
   dangerousUrl: string = '';
-  trustedUrl: any;
+  trustedUrl!: SafeUrl;
+  dangerousVideoUrl!: string;
+  videoUrl!: SafeResourceUrl;
   numbers: any;
+  htmlSnippet: string = '';
 
   constructor(private sanitizer: DomSanitizer) {
     this.numbers = of(1, 2, 3); // simple observable that emits three values
+    this.htmlSnippet = 'Template <script>alert("0wned")</script> <b>Syntax</b>';
   }
 
   ngOnInit(): void {
     this.dangerousUrl = 'javascript:alert("Hi there")';
     this.trustedUrl = this.sanitizer.bypassSecurityTrustUrl(this.dangerousUrl);
+    this.SanitizeVideoUrl('mVjYG9TSN88');
     this.numbers.subscribe({
       next(value: any) { console.log('Observable emitted the next value: ' + value); },
       error(err: any)  { console.error('Observable emitted an error: ' + err); },
       complete()  { console.log('Observable emitted the complete notification'); }
     });
+  }
+
+  SanitizeVideoUrl(id: string) {
+    this.dangerousVideoUrl = 'https://www.youtube.com/embed/' + id;
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
   }
 }
